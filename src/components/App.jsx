be33df8b02040087a8f7d3190265f40a7c47f2fb
RedokLeeroy <Lys1999@ukr.net>
2022-08-03 16:13:35 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import LoaderSpinner from "./Loader/Loader";
 import { Routes, Route } from "react-router-dom";
 import { Suspense, lazy } from "react";
 
-const MovieDetailsLazy = lazy(() => import("./Pages/MovieDetails"));
-const MovieSearchLazy = lazy(() => import("./Pages/MovieSearch"));
-const LayoutLazy = lazy(() => import("./Pages/Layout"));
-const TrendLazy = lazy(() => import("./Pages/Trends"));
-const CastLazy = lazy(() => import("./Pages/Cast"));
-const ReviewsLazy = lazy(() => import("./Pages/Reviews"));
+const MovieDetailsLazy = lazy(() => import("../Pages/MovieDetails"));
+const MovieSearchLazy = lazy(() => import("../Pages/MovieSearch"));
+const LayoutLazy = lazy(() => import("./Layout"));
+const TrendLazy = lazy(() => import("../Pages/Trends"));
+const CastLazy = lazy(() => import("./Cast"));
+const ReviewsLazy = lazy(() => import("./Reviews"));
 
 export const App = () => {
   return (
